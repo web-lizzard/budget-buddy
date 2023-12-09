c45ab5a3f2b37592ceffbddcf332ac242ac5997d
web-lizzard <czarnous.adrian@gmail.com>
2023-12-09 17:52:08 +0100

    feat: introduce settings with pydantic

---
+++ settings.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+from dotenv import find_dotenv
+
+
+class Database(BaseModel):
+    uri: str
+
+
+class Settings(BaseSettings):
+    database: Database
+
+    class Config:
+        env_nested_delimiter = "__"
+        extra = "ignore"
+        env_file = find_dotenv()
+
+
+settings = Settings()
