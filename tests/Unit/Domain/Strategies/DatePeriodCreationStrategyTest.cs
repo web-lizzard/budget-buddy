39defc013aa428b970f78a6f80cd6bbbd7035169
web-lizzard <czarnous.adrian@gmail.com>
2024-10-02 22:38:32 +0200

    test(Domain/Strategies): Add missing test for strategy validation

--- tests/Unit/Domain/Strategies/DatePeriodCreationStrategyTest.cs
+++ tests/Unit/Domain/Strategies/DatePeriodCreationStrategyTest.cs
@@ -1,58 +1,49 @@
-using BudgetBuddy.Domain.Ports;
+using BudgetBuddy.Domain.Exceptions;
 using BudgetBuddy.Domain.Strategies;
 using BudgetBuddy.Domain.ValueObjects;
 using Shouldly;
 
 namespace Unit.Domain.Strategies;
 
-public class DatePeriodCreationStrategyTest
+public class RegularDatePeriodComputingStrategyTest
 {
-    private readonly Clock _clock;
 
-    public DatePeriodCreationStrategyTest()
+    private readonly RegularMonthDatePeriodComputingStrategy _strategy;
+    public RegularDatePeriodComputingStrategyTest()
     {
-        _clock = new TestClock();
+        _strategy = new RegularMonthDatePeriodComputingStrategy();
     }
 
     [Fact]
     public void should_returns_true_when_input_match_to_schema_type()
     {
         var inputType = DatePeriodSchema.Type.NTH_REGULAR_DAY;
-        var strategy = new RegularMonthDatePeriodComputingStrategy();
 
-        var canApply = strategy.CanApply(inputType);
+        var canApply = _strategy.CanApply(inputType);
 
         canApply.ShouldBe(true);
     }
 
     [Fact]
-    public async void should_returns_computed_date_period_calculated_by_strategy()
+    public void should_fail_if_expected_day_is_above_breakpoint()
     {
-        var now = new Date(_clock.Current());
-        var schema = new DatePeriodSchema(5, DatePeriodSchema.Type.NTH_WORKING_DAY);
-        var strategy = new RegularMonthDatePeriodComputingStrategy();
-        var expectedDateRange = new DatePeriod(now, GetEndDate(5));
+        var record = Record.Exception(() => new DatePeriodSchema(29, DatePeriodSchema.Type.NTH_REGULAR_DAY));
 
-        var result = await strategy.ComputeDatePeriod(now, schema);
+        record.ShouldBeOfType<ExceededDayForPeriodSchemaException>();
+
+    }
+
+    [Theory]
+    [InlineData("2022-08-15", "2022-09-0", 6)]
+    [InlineData("2022-12-03", "2023-01-", 16)]
+    public async void should_returns_computed_date_period_calculated_by_strategy(string startDate, string endDate, int expectedEndDay)
+    {
+        var now = new Date(DateTime.Parse(startDate));
+        var schema = new DatePeriodSchema(expectedEndDay, DatePeriodSchema.Type.NTH_REGULAR_DAY);
+        var expectedDateRange = new DatePeriod(now, new Date(DateTime.Parse(endDate + expectedEndDay)));
+
+        var result = await _strategy.ComputeDatePeriod(now, schema);
 
         result.ShouldBe(expectedDateRange);
     }
-
-    private Date GetEndDate(int day)
-    {
-        return new Date(
-            new DateTime(2022, 09, day, 0, 0, 0)
-        );
-    }
-
-
-
-    private class TestClock : Clock
-    {
-        public DateTime Current()
-        {
-            return new(2022, 08, 11, 0, 0, 0);
-        }
-    }
-
 }
