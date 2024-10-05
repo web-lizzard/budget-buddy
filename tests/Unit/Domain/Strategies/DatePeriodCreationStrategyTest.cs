5e74b2f161aacc995753fd5cb2aa5798e0aacbf3
web-lizzard <czarnous.adrian@gmail.com>
2024-10-05 22:47:50 +0200

    test(Domain/Strategies): Test for ComputeDatePeriod for NTH working day

--- tests/Unit/Domain/Strategies/DatePeriodCreationStrategyTest.cs
+++ tests/Unit/Domain/Strategies/DatePeriodCreationStrategyTest.cs
@@ -1,3 +1,4 @@
+using BudgetBuddy.Adapters.Checkers;
 using BudgetBuddy.Domain.Exceptions;
 using BudgetBuddy.Domain.Strategies;
 using BudgetBuddy.Domain.ValueObjects;
@@ -55,18 +56,38 @@ public class WorkingDayDatePeriodComputingStrategyTest
 
     public WorkingDayDatePeriodComputingStrategyTest()
     {
-        _strategy = new WorkingDayDatePeriodComputingStrategy();
+        var offlineWorkingDayChecker = new OfflineWorkingDayChecker();
+        _strategy = new WorkingDayDatePeriodComputingStrategy(offlineWorkingDayChecker);
     }
 
     [Fact]
     public void should_returns_true_when_input_match_to_schema_type()
     {
-        // Given
         var inputType = DatePeriodSchema.Type.NTH_WORKING_DAY;
-        // When
         var canApply = _strategy.CanApply(inputType);
-        // Then
 
         canApply.ShouldBe(true);
     }
+
+    [Fact]
+    public void should_fail_if_expected_day_is_above_breakpoint()
+    {
+        var record = Record.Exception(() => new DatePeriodSchema(21, DatePeriodSchema.Type.NTH_WORKING_DAY));
+
+        record.ShouldBeOfType<ExceededDayForPeriodSchemaException>();
+    }
+
+    [Theory]
+    [InlineData("2022-11-26", "2022-12-27", 18)]
+    public async void should_returns_computed_date_period_calculated_by_strategy(string startDate, string endDate, int expectedEndDay)
+    {
+        var now = new Date(DateTime.Parse(startDate));
+        var schema = new DatePeriodSchema(expectedEndDay, DatePeriodSchema.Type.NTH_WORKING_DAY);
+        var expectedDateRange = new DatePeriod(now, new Date(DateTime.Parse(endDate)));
+
+        var result = await _strategy.ComputeDatePeriod(now, schema);
+
+        result.ShouldBe(expectedDateRange);
+    }
 }
+
